a9c5fa133f568c4b42fca407b2ea6eb2a1b6260d
frospirum_冻梨 <frospirum@outlook.com>
2021-06-14 09:35:48 +0800

    v0.4.7 update

--- init.lua
+++ init.lua
@@ -1,15 +1,13 @@
-packagename="com.oblisetta.arkhelper"
-user_permission={
-  [1]	= "ACCESS_COARSE_LOCATION" ;
-  [2]	= "ACCESS_FINE_LOCATION" ;
-  [3]	= "ACCESS_NETWORK_STATE" ;
-  [4]	= "ACCESS_WIFI_STATE" ;
-  [5]	= "INTERNET" ;
-  [6]	= "WRITE_EXTERNAL_STORAGE" ;
-  } ;
 appcode="210613"
 template="tool"
-appver="0.4.6"
+packagename="com.oblisetta.arkhelper"
+user_permission={
+  [4]	= "WRITE_EXTERNAL_STORAGE" ;
+  [1]	= "ACCESS_NETWORK_STATE" ;
+  [2]	= "ACCESS_WIFI_STATE" ;
+  [3]	= "INTERNET" ;
+  } ;
+appver="0.4.7"
 appname="Arkhelper"
 
 --require "import" import"loadlayout" import"loadmenu" import"loadbitmap" 
